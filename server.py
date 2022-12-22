9db7b737a33d6005aa30392c25e1ab952ca875a4
kandoriya3 <86428593+kandoriya3@users.noreply.github.com>
2022-12-22 23:56:06 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import rembg
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = '/path/to/upload/folder'
+UPLOAD_FOLDER = '/uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/', methods=['GET'])
