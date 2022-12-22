66886a6ee20778205a6c6ed62cce665ace950293
kandoriya3 <86428593+kandoriya3@users.noreply.github.com>
2022-12-23 02:20:53 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,16 +2,14 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/process', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
   if request.method == 'POST':
     # Get form data
     file = request.files['file']
-    print(f'Received file: {file}')  # Debug line
 
     # Process file
     result = process_file(file)
-    print(f'Result: {result}')  # Debug line
 
     # Render result template
     return render_template('result.html', result=result)
@@ -20,9 +18,6 @@ def index():
     return render_template('index.html')
 
 def process_file(file):
-  # Debug line
-  import pdb; pdb.set_trace()
-
   # Process file
   result = file.filename
   return result
