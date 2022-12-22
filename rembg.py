3b307df0ad2b39257147c73d7ae9002b08e1f019
kandoriya3 <86428593+kandoriya3@users.noreply.github.com>
2022-12-22 19:14:14 +0530

    Update rembg.py

--- rembg.py
+++ rembg.py
@@ -1,7 +1,9 @@
 # rembg.py
 
+import base64
+import cv2
+import numpy as np
 from flask import Flask, request
-from PIL import Image
 
 app = Flask(__name__)
 
@@ -11,16 +13,29 @@ def rembg():
   data_url = request.data
 
   # Load the image from the data URL
-  image = Image.open(BytesIO(base64.b64decode(data_url)))
+  image_data = base64.b64decode(data_url.split(",")[1])
+  image = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_COLOR)
 
-  # Process the image to remove the background
-  # (Replace this with your own code using OpenCV or Pillow)
-  processed_image = image.copy()
+  # Convert the image to grayscale and apply a threshold
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 
-  # Save the processed image as a data URL
-  buffer = BytesIO()
-  processed_image.save(buffer, format="JPEG")
-  processed_data_url = f"data:image/jpeg;base64,{base64.b64encode(buffer.getvalue()).decode()}"
+  # Find the contours of the foreground objects
+  cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+  # Create a mask with the same size as the image and fill it with white
+  mask = np.ones(image.shape[:2], dtype=np.uint8) * 255
+
+  # Draw the contours on the mask
+  cv2.drawContours(mask, cnts, -1, 0, -1)
+
+  # Apply the mask to the image
+  processed_image = cv2.bitwise_and(image, image, mask=mask)
+
+  # Encode the processed image as a JPEG and return it as a data URL
+  _, jpeg_data = cv2.imencode(".jpg", processed_image)
+  processed_data_url = f"data:image/jpeg;base64,{base64.b64encode(jpeg_data).decode()}"
 
   # Return the processed image data URL as the response
   return processed_data_url
