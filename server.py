2acf22146ddc2385180961594fb9005995793e97
kandoriya3 <86428593+kandoriya3@users.noreply.github.com>
2022-12-23 02:15:56 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/process', methods=['GET', 'POST'])
 def index():
   if request.method == 'POST':
     # Get form data
