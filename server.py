1d3af308c634b448900dc0353dfa90103b7f1bc0
kandoriya3 <86428593+kandoriya3@users.noreply.github.com>
2022-12-23 00:49:04 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import rembg
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = '/super-umbrella/uploads/'
+UPLOAD_FOLDER = '/uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/', methods=['GET'])
