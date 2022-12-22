42b12f41180381da75b4841d0b6a7f8e47fea4b6
kandoriya3 <86428593+kandoriya3@users.noreply.github.com>
2022-12-22 23:42:25 +0530

    Update rembg.py

--- rembg.py
+++ rembg.py
@@ -1,43 +1,41 @@
 import cv2
-import base64
-import io
+import numpy as np
 
-def pipeline(input_image):
-    # Gaussian blur
-    blur = cv2.GaussianBlur(input_image, (5, 5), 0)
+def gaussian_blur(image, kernel_size=5):
+  return cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
 
-    # Edge detection
-    edges = cv2.Canny(blur, 75, 200)
+def edge_detection(image, threshold1=50, threshold2=150):
+  return cv2.Canny(image, threshold1, threshold2)
 
-    # Median filter
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    median = cv2.medianBlur(edges, 3)
+def median_filter(image, kernel_size=3):
+  return cv2.medianBlur(image, kernel_size)
 
-    # Dilate
-    dilate = cv2.dilate(median, kernel, iterations=5)
+def find_contours(image):
+  _, contours, _ = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  return contours
 
-    # Find significant contours
-    _, contours, _ = cv2.findContours(dilate, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    mask = np.zeros(input_image.shape, dtype=np.uint8)
-    cv2.drawContours(mask, contours, 0, (255, 255, 255), -1)
+def mask_background(image, contours):
+  mask = np.zeros_like(image)
+  cv2.drawContours(mask, contours, -1, 255, -1)
+  return mask
 
-    # Mask probable background
-    mask = cv2.bitwise_not(mask)
-    output_image = cv2.bitwise_and(input_image, input_image, mask=mask)
-
-    return output_image
-
-def process_image(input_path, output_path):
-    # Load the input image
-    input_image = cv2.imread(input_path)
-
-    # Run the image processing pipeline
-    output_image = pipeline(input_image)
-
-    # Encode the output image as a PNG
-    png_bytes = io.BytesIO()
-    cv2.imwrite(png_bytes, output_image)
-
-    # Return the output image as a data URL
-    return "data:image/png;base64," + base64.b64encode(png_bytes.getvalue()).decode()
+def remove_background(image):
+  # Step 1: Gaussian Blur
+  image = gaussian_blur(image)
+  
+  # Step 2: Edge Detection
+  edges = edge_detection(image)
+  
+  # Step 3: Filter Out Salt and Pepper Noise using Median Filter
+  edges = median_filter(edges)
+  
+  # Step 4: Find Significant Contours
+  contours = find_contours(edges)
+  
+  # Step 5: Masking Probable Background
+  mask = mask_background(image, contours)
+  
+  # Apply mask to input image
+  result = cv2.bitwise_and(image, mask)
+  
+  return result
